c30391e9c9d5c636ed9c7ee6592a26fbb5361451
Vinícius Dutra <54730460+ViniciusDutt@users.noreply.github.com>
2024-09-25 00:05:28 -0300

    fix: WinModal message

--- src/components/WinModal.tsx
+++ src/components/WinModal.tsx
@@ -9,7 +9,7 @@ const WinModal = ({ isOpen, tipCount, playCount }: any) => {
       <div className="w-full max-w-[512px] gap-6 flex flex-col relative mx-2 bg-background-200 px-6 py-6 rounded-xl">
         <h1 className="text-2xl font-bold">Parabéns!</h1>
         <p className="text-base">
-          Você acertou em {playCount} tentativas.
+          {tipCount === 0 && <p>Você acertou em {playCount} tentativas.</p>}
           {tipCount > 0 && (
             <p>
               Você acertou em {playCount} tentativas usando {tipCount} dicas.
